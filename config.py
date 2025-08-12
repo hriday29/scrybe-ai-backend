7ae557a1bd4dba23f47c9b8329fe4ec22f8ae48f
hriday29 <vighriday@gmail.com>
2025-08-12 19:41:52 +0530

    Fixing before 1 year backtest

--- config.py
+++ config.py
@@ -18,6 +18,11 @@ GEMINI_API_KEY_POOL = [
         os.getenv("GOOGLE_API_KEY_3"),
         os.getenv("GOOGLE_API_KEY_4"),
         os.getenv("GOOGLE_API_KEY_5"),
+        os.getenv("GOOGLE_API_KEY_6"),
+        os.getenv("GOOGLE_API_KEY_7"),
+        os.getenv("GOOGLE_API_KEY_8"),
+        os.getenv("GOOGLE_API_KEY_9"),
+        os.getenv("GOOGLE_API_KEY_10"),
     ] if key
 ]
 
